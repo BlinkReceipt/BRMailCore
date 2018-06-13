ff62f242490496b6d10b5ec05520297f0ce53406
Danny Panzer <danny.panzer@gmail.com>
2018-06-13 14:31:37 -0400

    v0.0.1

---
+++ MailCore.framework/PrivateHeaders/BRMailCore.h
@@ -0,0 +1,19 @@
+//
+//  BRMailCore.h
+//  BRMailCore
+//
+//  Created by Danny Panzer on 6/12/18.
+//  Copyright © 2018 BlinkReceipt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BRMailCore.
+FOUNDATION_EXPORT double BRMailCoreVersionNumber;
+
+//! Project version string for BRMailCore.
+FOUNDATION_EXPORT const unsigned char BRMailCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BRMailCore/PublicHeader.h>
+
+
